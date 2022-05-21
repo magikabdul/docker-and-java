659619b18e4809c6d310012b924da51a2498e569
Krzysztof Cholewa - Ubuntu <krzysztof.cholewa@gmail.com>
2022-05-21 22:14:21 +0200

    example of pullImage

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,24 @@
 import com.github.dockerjava.api.DockerClient;
 import docker.DockerInstance;
+import lombok.extern.slf4j.Slf4j;
 import service.DockerService;
 
+@Slf4j
 public class Application {
+    private static String LOG_PATTERN = "DOCKER method: %s test";
 
     public static void main(String[] args) {
-//        DockerClient dockerClient = new DockerInstance().getClient();
-//
-//        dockerClient.pingCmd().exec();
-        new DockerService().start();
+        log.info("Application start");
+        DockerService dockerService = new DockerService();
+
+        logCommand("pingCmd");
+        dockerService.ping();
+
+        logCommand("pullImage");
+        dockerService.pullImage();
+    }
+
+    private static void logCommand(String command) {
+        log.info(String.format(LOG_PATTERN, "\"" + command + "\""));
     }
 }
--- src/main/java/service/DockerService.java
+++ src/main/java/service/DockerService.java
@@ -1,20 +1,31 @@
 package service;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.command.PullImageResultCallback;
+import com.github.dockerjava.api.model.PullResponseItem;
 import docker.DockerInstance;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class DockerService {
 
     private final DockerClient dockerClient = new DockerInstance().getClient();
 
-    @SneakyThrows
-    public void start() {
-        dockerClient.pullImageCmd("openjdk:11-jdk").exec(new PullImageResultCallback()).awaitCompletion();
+    public void ping() {
+        dockerClient.pingCmd().exec();
+    }
 
-//        dockerClient.createContainerCmd("openjdk:11-jdk")
-//                .withCmd("ls")
-//                .exec();
+    @SneakyThrows
+    public void pullImage() {
+        PullImageResultCallback resultCallback = new PullImageResultCallback();
+        dockerClient.pullImageCmd("alpine:latest").exec(new PullImageResultCallback() {
+            @Override
+            public void onNext(PullResponseItem item) {
+                super.onNext(item);
+                System.out.println(item);
+            }
+        }).awaitCompletion();
     }
 }
